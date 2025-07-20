bffc83a19cfadeaec2dfec2067a2981a89d1ec80
surajsingh9389 <sk9389623488@gmail.com>
2025-07-20 23:02:22 +0530

    deploy

--- server/api/index.js
+++ server/api/index.js
@@ -10,12 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //Middleware
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 //Routes
